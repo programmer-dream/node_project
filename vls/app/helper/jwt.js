16c377f4ab66e216ace3a90c95a2801963a11246
Harshit Vaid <harshitvaid000@gmail.com>
2020-12-11 15:14:53 +0530

    added algo in JWT

--- vls/app/helper/jwt.js
+++ vls/app/helper/jwt.js
@@ -6,7 +6,7 @@ module.exports = jwt;
 
 function jwt() {
     const secret = config.secret;
-    return expressJwt({ secret, isRevoked }).unless({
+    return expressJwt({ secret, isRevoked, algorithms: ['RS256'] }).unless({
         path: [
             // public routes that don't require authentication
             '/auth/signin',
