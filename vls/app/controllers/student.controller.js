a55cd28e28330d7715a72d20538ff36142f5f1bd
Harshit Vaid <harshitvaid000@gmail.com>
2020-12-16 11:31:01 +0530

    changes in code according to new things

--- vls/app/controllers/student.controller.js
+++ vls/app/controllers/student.controller.js
@@ -8,93 +8,11 @@ const sequelize = db.sequelize;
 const bcrypt = require("bcryptjs");
 
 module.exports = {
-  create,
-  view,
-  list,
-  update,
-  studentDelete,
-  bulkDelete
-};
-
-async function create(req){
-	const t = await sequelize.transaction();
-	const errors = validationResult(req);
-
-	try {
-	   if(errors.array().length)
-   	  	 return { success: false, message: errors.array() };
-	   	   	 if(req.file){
-				req.body.profilepic = req.file.filename;
-			 }
-			const student = await StudentSchoolPersonal.create(req.body,{ transaction: t });
-
-			let studentId = student.studentSchoolVlsId
-			req.body.studentVlsId = studentId
-			let password = bcrypt.hashSync(req.body.password, 8);
-			let user_id = Date.now()
-			let auth = {
-				userType:"Student",
-				userVlsId:studentId,
-				userId: user_id,
-				password:password,
-				oldPassword1:password
-			};
-			let created_student = student.toJSON()
-			created_student.userId = user_id;
-			await StudentPersonal.create(req.body,{ transaction: t });
-			await Authentication.create(auth,{ transaction: t });
-
-			await t.commit();
-			return { success: true , message: 'Student was successfully created.',data: created_student};
-
-   	   
-	} catch (error) {
-	   await t.rollback();
-	   return { 
-	   						success: false,
-	   						message: error.message 
-	   					};
-	}
-};
-
-async function list(){
-  let list =  await StudentSchoolPersonal.findAll()
-  return { success: true,message: 'Student listing',data:list};
+  view
 };
 
 async function view(id){
   if(!id) return { success: false,message: 'Student not found' };
   	let school = await StudentSchoolPersonal.findByPk(id)
   	return { success: true, message: "Student data", data : school};
-};
-
-async function update(req){
-   if(req.file){
-	  req.body.profilepic = req.file.filename;
-   }
-   let num = await StudentSchoolPersonal.update(req.body, {
-    where: { studentSchoolVlsId: req.params.id }
-  })
-   if(num != 1) throw 'Student not updated'
-   let student  = await StudentSchoolPersonal.findByPk(req.params.id)
-    return {
-      success: true,
-      message: "Student updated successfully.",
-      data:student
-    }
-};
-async function studentDelete(id){
-  let num = await StudentSchoolPersonal.destroy({
-    where: { studentSchoolVlsId: id }
-  })
-	 if(num != 1) throw 'Student not deleted'
-	 return {success:true,message:'Student deleted successfully'}
-};
-async function bulkDelete(body){
-  if(!Array.isArray(body.ids)) throw 'ids index must be an array'
-  let num = await StudentSchoolPersonal.destroy({
-    where: { studentSchoolVlsId: body.ids }
-  })
-  if(num != 1) throw 'Student not deleted'
-  return {success:true,message:'Student deleted successfully'}
 };
--- vls/app/routes/student.routes.js
+++ vls/app/routes/student.routes.js
@@ -9,67 +9,14 @@ const router = express.Router()
 const app = express();
 app.use(bodyParser.json());
 
-//Post route
-router.post("/create",[
-    upload.single('profilepic'),
-    check('firstName', 'firstName field is required.').not().isEmpty(),
-    check('dob', 'dob field is required.').not().isEmpty(),
-    check('contact1', 'contact1 field is required.').not().isEmpty(),
-    check('fatherName', 'fatherName field is required.').not().isEmpty(),
-    check('motherName', 'motherName field is required.').not().isEmpty(),
-    check('address', 'Address field is required.').not().isEmpty(),
-    check('password', 'password field is reuired.').not().isEmpty()
-],create);
-
 //Get route
 router.get("/view/:id" ,view);
-router.get("/list" ,list);
 
-//Put route
-router.put("/update/:id",[
-    upload.single('profilepic'),
-    check('firstName', 'firstName field is required.').not().isEmpty(),
-    check('dob', 'dob field is required.').not().isEmpty(),
-    check('contact1', 'contact1 field is required.').not().isEmpty(),
-    check('fatherName', 'fatherName field is required.').not().isEmpty(),
-    check('motherName', 'motherName field is required.').not().isEmpty(),
-    check('address', 'Address field is required.').not().isEmpty(),
-    check('password', 'password field is reuired.').not().isEmpty()
-],update);
-
-//Delete route
-router.delete("/delete/:id" ,studentDelete);
-router.delete("/bulkDelete/" ,bulkDelete);
   
 module.exports = router;
 
-function create(req, res, next) {
-    studentController.create(req)
-        .then(student => student ? res.json(student) : res.status(400).json({ status: "error", message: 'Error while creating student'}))
-        .catch(err => next(err));
-}
 function view(req, res, next) {
     studentController.view(req.params.id)
         .then(student => student ? res.json(student) : res.status(400).json({ status: "error", message: 'Error while view student'}))
-        .catch(err => next(err));
-}
-function list(req, res, next) {
-    studentController.list(req)
-        .then(student => student ? res.json(student) : res.status(400).json({ status: "error", message: 'Error while list student'}))
-        .catch(err => next(err));
-}
-function update(req, res, next) {
-    studentController.update(req)
-        .then(student => student ? res.json(student) : res.status(400).json({ status: "error", message: 'Error while update student'}))
-        .catch(err => next(err));
-}
-function studentDelete(req, res, next) {
-    studentController.studentDelete(req.params.id)
-        .then(student => student ? res.json(student) : res.status(400).json({ status: "error", message: 'Error while delete student'}))
-        .catch(err => next(err));
-}
-function bulkDelete(req, res, next) {
-    studentController.bulkDelete(req.body)
-        .then(student => student ? res.json(student) : res.status(400).json({ status: "error", message: 'Error while delete student'}))
         .catch(err => next(err));
 }
