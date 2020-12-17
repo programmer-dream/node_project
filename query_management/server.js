c6a6f03c18f2db3364080c9ca95e108d20a20879
Harshit Vaid <harshitvaid000@gmail.com>
2020-12-17 15:51:48 +0530

    add query management

---
+++ query_management/server.js
@@ -0,0 +1,43 @@
+require('rootpath')();
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const jwt = require('./app/helper/jwt');
+const errorHandler = require('./app/helper/error-handler');
+require('dotenv').config()
+
+const app = express();
+
+var corsOptions = {
+  origin: "*"
+};
+
+app.use(express.static(path.join(__dirname, 'uploads')));
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// const db = require("./app/models");
+// db.sequelize.sync();
+app.get("/",function(req, res){
+	res.send('landing page');
+});
+
+// api routes
+
+
+// global error handler
+app.use(errorHandler);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
