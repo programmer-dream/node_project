5ee7c87febd69e50c709ab1cfc6b7209b1eb7948
vikas sharma <vikass185@gmail.com>
2020-12-24 11:15:41 +0530

    rating like eror

--- learning_library/app/controllers/library.controller.js
+++ learning_library/app/controllers/library.controller.js
@@ -185,6 +185,7 @@ function formatDate() {
  */
 async function getRatingLikes(id, user) {
   try{
+    let avg = null
     //get like count
     let like  = await Ratings.count({
       where:{likes:1,learning_library_vls_id:id}
@@ -196,10 +197,11 @@ async function getRatingLikes(id, user) {
       where:{learning_library_vls_id:id},
       group:['learning_library_vls_id']
     })
+    if(ratings){
     //get rating & likes
-    let ratingData = ratings.toJSON();
-    let avg = parseInt(ratingData.total_ratings) / ratingData.total_count
-
+      let ratingData = ratings.toJSON();
+        avg          = parseInt(ratingData.total_ratings) / ratingData.total_count
+    } 
     userRating  = await Ratings.findOne({
       attributes: ['ratings','likes'],
       where:{learning_library_vls_id:id,user_vls_id:user.userVlsId}
