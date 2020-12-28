34cc9635b944f059ef90dd8d16b0ca9fd48756b0
Harshit Vaid <harshitvaid000@gmail.com>
2020-12-28 17:57:07 +0530

    added avg in rattings

--- query_management/app/controllers/ratings.controller.js
+++ query_management/app/controllers/ratings.controller.js
@@ -22,6 +22,21 @@ async function addUpdateRatings(req){
     let userEntry = await Ratings.findOne({ where : { user_vls_id : req.body.user_vls_id, query_vls_id : req.body.query_vls_id } })
     console.log(userEntry,"userEntry")
 
+    //get rating avg
+    let ratingsData = await Ratings.findOne({
+      attributes: [[Sequelize.fn('SUM', Sequelize.col('ratings')), 'total_ratings'],
+      [Sequelize.fn('COUNT', Sequelize.col('ratings')), 'total_count']],
+      where:{ query_vls_id: req.body.query_vls_id },
+      group:['query_vls_id']
+    })
+    let avg = null
+
+    if(ratingsData){
+    //get rating & likes
+      let ratingData = ratingsData.toJSON();
+      avg = parseInt(ratingData.total_ratings) / ratingData.total_count
+    }
+
     let rattings 
     let message 
     let data
@@ -29,10 +44,12 @@ async function addUpdateRatings(req){
       rattings = await Ratings.create(req.body);
       message = 'Rating created successfully'
       data = rattings
+      data.avg = avg
     }else{
       userEntry.update(req.body)
       message = 'Rating updated successfully'
       data = req.body
+      data.avg = avg
     }
     
     return { success: true, message: message, data:data };
