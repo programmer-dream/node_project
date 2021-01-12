160ba85c25a6457a0c1c4961135a90d1904e89c9
Harshit Vaid <harshitvaid000@gmail.com>
2021-01-12 15:57:15 +0530

    changes in timetable for subject_id to subject_code

--- time_sheet/app/controllers/timeSheet.controller.js
+++ time_sheet/app/controllers/timeSheet.controller.js
@@ -31,7 +31,7 @@ async function create(req){
   //if(req.user.role != 'teacher' || req.user.role != 'principle') throw 'Unauthorized User'
   let timetable = req.body.timetable
   let timings   = await checkEnterTimings(timetable)
-  return 'out'
+
   let timeData  = []
 
   user          = await User.findByPk(req.user.id)
@@ -46,7 +46,7 @@ async function create(req){
     timetable.map(async timetable => {
         let status     = 1
         let section_id = 0
-        if(!timetable.subject_id) throw 'subject_id required'
+        if(!timetable.subject_code) throw 'subject_code required'
         if(!timetable.start_time) throw 'start_time required'
         if(!timetable.end_time)   throw 'end_time required'
         if(!timetable.room_no)   throw 'room_no required'
@@ -61,6 +61,7 @@ async function create(req){
             status     = 0
         if(timetable.section_id)
             section_id = timetable.section_id
+
         let body = {
               day              : moment(req.body.day, 'd').format('dddd'),
               class_id         : req.body.class_id,
@@ -71,12 +72,12 @@ async function create(req){
               section_id       : section_id,
               room_no          : timetable.room_no, 
               status           : status,
-              subject_id       : timetable.subject_id,
+              subject_code     : timetable.subject_code,
               start_time       : timetable.start_time,
               end_time         : timetable.end_time
           }
          
-          await getScheduleData(user.school_id, req.body.class_id, section_id, timetable.subject_id, timetable.start_time, timetable.end_time ,body.day)
+          await getScheduleData(user.school_id, req.body.class_id, section_id, timetable.subject_code, timetable.start_time, timetable.end_time ,body.day)
 
             timeData.push(body)
     })
@@ -198,6 +199,8 @@ async function update(req){
   const errors = validationResult(req);
   if(errors.array().length) throw errors.array()
   let timetable = req.body.timetable
+  let timings   = await checkEnterTimings(timetable)
+
   let timeData  = []
 
   user          = await User.findByPk(req.user.id)
@@ -219,7 +222,7 @@ async function update(req){
     timetable.map(async timetable => {
         let status     = 1
         let section_id = 0
-        if(!timetable.subject_id) throw 'subject_id required'
+        if(!timetable.subject_code) throw 'subject_code required'
         if(!timetable.start_time) throw 'start_time required'
         if(!timetable.end_time)   throw 'end_time required'
         if(!timetable.room_no)   throw 'room_no required'
@@ -237,10 +240,13 @@ async function update(req){
               section_id       : section_id,
               room_no          : timetable.room_no, 
               status           : status,
-              subject_id       : timetable.subject_id,
+              subject_code     : timetable.subject_code,
               start_time       : timetable.start_time,
               end_time         : timetable.end_time
           }
+
+          await getScheduleData(user.school_id, req.body.class_id, section_id, timetable.subject_code, timetable.start_time, timetable.end_time ,body.day)
+
         timeData.push(body)
     })
   )
@@ -252,7 +258,7 @@ async function update(req){
 /**
  * method to check if schedule exist
  */
-async function getScheduleData(school_id, class_id, section_id, subject_id, start_time, end_time, day){
+async function getScheduleData(school_id, class_id, section_id, subject_code, start_time, end_time, day){
  
   let routines = await Routine.findAll({
     where : {
@@ -261,19 +267,19 @@ async function getScheduleData(school_id, class_id, section_id, subject_id, star
               school_vls_id  : school_id,
               day            : day
           },
-          attributes: ['subject_id','start_time','end_time']
+          attributes: ['subject_code','start_time','end_time']
   })
    await Promise.all(
       routines.map(async routine => {
       if(routine.start_time === start_time)
-         throw `subject_id ${routine.subject_id} timings are overlapping with subject_id ${subject_id}`
+         throw `subject_code ${routine.subject_code} timings are overlapping with subject_code ${subject_code}`
     
       let time       = moment(start_time, 'hh:mm')
       let beforeTime = moment(routine.start_time, 'hh:mm')
       let afterTime  = moment(routine.end_time, 'hh:mm')
       
       if (time.isBetween(beforeTime, afterTime)) 
-          throw `subject_id ${routine.subject_id} timings are overlapping with subject_id ${subject_id}`  
+          throw `subject_code ${routine.subject_code} timings are overlapping with subject_code ${subject_code}`  
     })
   )
   
@@ -287,14 +293,14 @@ async function checkEnterTimings(timetable){
         for(let i = 0; i < length; i++ ){
           if (timetable[index] != timetable[i]) {
             if(timetable[index].start_time === timetable[i].start_time)
-              throw `subject_id ${timetable[index].subject_id} timings are overlapping with subject_id ${timetable[i].subject_id}`
+              throw `subject_code ${timetable[index].subject_code} timings are overlapping with subject_code ${timetable[i].subject_code}`
             
             let time       = moment(timetable[index].start_time, 'hh:mm')
             let beforeTime = moment(timetable[i].start_time, 'hh:mm')
             let afterTime  = moment(timetable[i].end_time, 'hh:mm')
             
             if (time.isBetween(beforeTime, afterTime)) 
-                throw `subject_id ${timetable[index].subject_id} timings are overlapping with subject_id ${timetable[i].subject_id}`
+                throw `subject_code ${timetable[index].subject_code} timings are overlapping with subject_code ${timetable[i].subject_code}`
           }
         }
     })
