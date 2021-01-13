196204c7cd0e9fe88aeb84565d9cf7b5adcb38f7
Harshit Vaid <harshitvaid000@gmail.com>
2021-01-13 12:02:42 +0530

    added new role and user for school admin

--- seeders/20210111124144-add_demo_subject.js
+++ seeders/20210111124144-add_demo_subject.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('subject_list', null, {});
-     await queryInterface.sequelize.query("INSERT INTO `subject_list` (`id`, `subject_name`, `code`, `school_vls_id`, `branch_vls_id`, `created_at`, `updated_at`) VALUES(1, 'Hindi', '001', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00'),(2, 'English', '002', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00'),(3, 'Punjabi', '003', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00'),(4, 'Subject 1', '004', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00')")
+     await queryInterface.sequelize.query("INSERT INTO `subject_list` (`id`, `subject_name`, `code`, `school_vls_id`, `branch_vls_id`, `created_at`, `updated_at`) VALUES(1, 'Hindi', '001', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00'),(2, 'English', '002', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00'),(3, 'Punjabi', '003', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00'),(4, 'Subject 1', '004', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00'),(5, 'Punjabi', '005', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00'),(6, 'English', '006', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00'),(7, 'Math', '007', 1, 1, '2021-01-05 00:00:00', '2021-01-05 00:00:00')")
   },
 
   down: async (queryInterface, Sequelize) => {
