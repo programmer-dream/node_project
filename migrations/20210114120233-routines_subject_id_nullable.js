a8b788cc4dd684a052e9ac29fff23deb13f0f388
vikas sharma <vikass185@gmail.com>
2021-01-14 17:36:09 +0530

    time sheet changes

---
+++ migrations/20210114120233-routines_subject_id_nullable.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('routines', 'subject_id', {
+                      type: Sequelize.INTEGER
+                  });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    
+  }
+};
