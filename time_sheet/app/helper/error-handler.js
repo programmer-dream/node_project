8a1d9d6d3021abf79f35e5afbbe5e42e7af78615
Harshit Vaid <harshitvaid000@gmail.com>
2021-01-20 17:49:40 +0530

    fixed issue with error handling token

--- time_sheet/app/helper/error-handler.js
+++ time_sheet/app/helper/error-handler.js
@@ -3,6 +3,11 @@ module.exports = errorHandler;
 
 function errorHandler(err, req, res, next) {
 
+    if (err.name === 'UnauthorizedError') {
+        // jwt authentication error
+        return res.status(401).json({ status: "error", message: err });
+    }
+
     if (err instanceof ReferenceError) {
         console.log(err, "err err err err")
         // custom application error
@@ -31,10 +36,6 @@ function errorHandler(err, req, res, next) {
         return res.status(400).json({ status: "error", message: err.message });
     }
 
-    if (err.name === 'UnauthorizedError') {
-        // jwt authentication error
-        return res.status(401).json({ status: "error", message: 'Invalid Token' });
-    }
 
     // default to 500 server error
     return res.status(500).json({ status: "error", message: err.message });
