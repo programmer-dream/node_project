a15c05c3fc0aacff6f191d879de95aaacba92d31
Harshit Vaid <harshitvaid000@gmail.com>
2021-01-27 16:58:07 +0530

    removed commented code for migration

--- migrations/20210127071334-update_assignment_question.js
+++ migrations/20210127071334-update_assignment_question.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-        // await queryInterface.removeColumn('assignment_questions', 'branch_vls_id');
-        // await queryInterface.removeColumn('assignment_questions', 'school_vls_id');
-        // await queryInterface.removeColumn('assignment_questions', 'student_vls_id');
-        // await queryInterface.removeColumn('assignment_questions', 'next_assignment_question_id');
+        await queryInterface.removeColumn('assignment_questions', 'branch_vls_id');
+        await queryInterface.removeColumn('assignment_questions', 'school_vls_id');
+        await queryInterface.removeColumn('assignment_questions', 'student_vls_id');
+        await queryInterface.removeColumn('assignment_questions', 'next_assignment_question_id');
         await queryInterface.changeColumn('assignment_questions', 'question_type', {
                       type: Sequelize.ENUM('form','choice','multiple_checklist'),
                       allowNull: false
