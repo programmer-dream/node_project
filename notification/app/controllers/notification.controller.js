889fad90ff32e4a19f587c3ae6f9bdfcb81ee5b2
vikas sharma <vikass185@gmail.com>
2021-03-01 18:11:21 +0530

    users changes

--- notification/app/controllers/notification.controller.js
+++ notification/app/controllers/notification.controller.js
@@ -72,7 +72,7 @@ async function list(params , user){
       allNotifications.push(notification)
     })
   )
-  
+
   allNotifications.sort(function(a,b) {
     return b.notification_vls_id - a.notification_vls_id;
   });
@@ -220,6 +220,10 @@ async function create(req){
 
     let user = req.user
     let customNotification = req.body
+
+    if(customNotification.users)
+        customNotification.users = JSON.stringify(customNotification.users)
+
     customNotification.notificaton_type = 'custom_notification'
     customNotification.event_type       = 'created'
     customNotification.notificaton_type_id = 0
