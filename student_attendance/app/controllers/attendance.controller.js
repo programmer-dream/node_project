f4fa051d79616f3180650b4a8f14f60825e816b5
vikas sharma <vikass185@gmail.com>
2021-03-04 13:31:36 +0530

    attendance changes

--- student_attendance/app/controllers/attendance.controller.js
+++ student_attendance/app/controllers/attendance.controller.js
@@ -1070,7 +1070,10 @@ async function studentCount(userId, currentYear, currentMonth, currentDay){
 			 				absentCount++
 			 			}
 			 		}
-			 		subjectWistAttendance[subject] = {present : presentCount, absent : absentCount}
+			 		if(!subjectWistAttendance['subjects'])
+			 			subjectWistAttendance['subjects'] = {}
+			 		
+			 		subjectWistAttendance['subjects'][subject] = {present : presentCount, absent : absentCount}
 		 		})	
 			)
 		}
