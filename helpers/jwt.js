b3b09fbc377d11c09aa998c3b4eadb928559f291
Harshit Vaid <harshitvaid000@gmail.com>
2021-03-19 18:43:16 +0530

    changes in jwt site

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,8 +2,6 @@ const expressJwt = require('express-jwt');
 const config = require("../config/env.js");
 const authController = require("../vls/app/controllers/auth.controller");
 
-module.exports = jwt;
-
 function jwt() {
     const secret = config.secret;
     return expressJwt({ secret, isRevoked, algorithms: ['sha1', 'RS256', 'HS256'] }).unless({
@@ -28,3 +26,5 @@ async function isRevoked(req, payload, done) {
 
     done();
 };
+
+module.exports = jwt;
