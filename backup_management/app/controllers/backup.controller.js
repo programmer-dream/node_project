eacb6e670e38938c55c416cb85c28a22aa447d57
vikas sharma <vikass185@gmail.com>
2021-03-23 18:32:39 +0530

    export database

--- backup_management/app/controllers/backup.controller.js
+++ backup_management/app/controllers/backup.controller.js
@@ -4,6 +4,7 @@ const moment 	 = require("moment");
 const bcrypt     = require("bcryptjs");
 const path       = require('path')
 const mysqlDump  = require('mysqldump')
+const Importer = require('mysql-import');
 const fs 		 = require('fs');
 const Op 	 	 = db.Sequelize.Op;
 const Sequelize  = db.Sequelize;
@@ -13,7 +14,8 @@ const AcademicYear  = db.AcademicYear;
 
 module.exports = {
   academicYears,
-  exportData
+  exportData,
+  importData
 };
 
 
@@ -33,6 +35,8 @@ async function academicYears(params , user){
  * API for  export data
  */
 async function exportData(params , user){
+	let date = moment().format('YYYY_MM_DD')
+	
 	mysqlDump({
     connection: {
         host: 'localhost',
@@ -40,19 +44,50 @@ async function exportData(params , user){
         password: 'Password@123',
         database: 'dev_vls'
 	    },
-	    dumpToFile: './uploads/dump.sql.gz',
-	    compressFile: true,
-	    dump:{
-	    	tables:['assignment']
-	    },
-	    dataDump:{
-	    	where:{
-	    		'assignment':'assignment_vls_id < 40'
+	dumpToFile: './uploads/dump_'+date+'.sql',
+	dump:{
+	    	schema : false,
+	    	//tables:['assignment']
+	    	data:{
+	    		maxRowsPerInsertStatement:100,
+	    		where:{
+	    			'assignment' : 'school_vls_id = 1'
+	    		}
 	    	}
-	    }
+	}
 	});
 	
-  return { success: true, message: "Data exported "}
+  return { success: true, message: "Data exported"}
 };
 
+/**
+ * API for  import data
+ */
+async function importData(params , user){	
+	const host 		= 'localhost';
+	const dbuser 	= 'root';
+	const password 	= 'Password@123';
+	const database 	= 'backup_vls';
 
+	// exec(`mysql -u${importTo.user} -p${importTo.password} -h ${importTo.host} ${importTo.database} < ${dumpFile}`, (err, stdout, stderr) => {
+ //        if (err) { console.error(`exec error: ${err}`); return; }
+
+ //        console.log(`The import has finished.`);
+	// });
+	const uploadsFolder = './uploads/';
+
+	// let allFiles = []
+	// fs.promise.readdir(uploadsFolder, (err, files) => {
+	// 	allFiles.push(files)
+	// 	console.log(allFiles)
+	// })
+	let allFiles = []
+	let files = fs.readdirSync(uploadsFolder);
+
+	files.forEach(file => {
+		if(file.includes('.sql'))
+	    	allFiles.push(file);
+	});
+
+ 	return { success: true, message: "Data imported ", data : allFiles }
+};
--- backup_management/app/routes/backup.routes.js
+++ backup_management/app/routes/backup.routes.js
@@ -9,6 +9,7 @@ const upload  = helper.upload;
 //Get 
 router.get("/academicYears/",academicYears);
 router.get("/exportData/",exportData);
+router.post("/importData/",importData);
 
 module.exports = router;
 
@@ -22,6 +23,13 @@ function academicYears(req, res, next) {
 // Function for export data
 function exportData(req, res, next) {
     backupController.exportData(req.query, req.user)
+        .then(academic => academic ? res.json(academic) : res.status(400).json({ status: "error", message: 'Error while listing academic year' }))
+        .catch(err => next(err));
+}
+
+// Function for import data
+function importData(req, res, next) {
+    backupController.importData(req.query, req.user)
         .then(academic => academic ? res.json(academic) : res.status(400).json({ status: "error", message: 'Error while listing academic year' }))
         .catch(err => next(err));
 }
