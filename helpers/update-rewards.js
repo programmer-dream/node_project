4f88ed1eddd509efe696548f55e97fc4b9c4b746
vikas sharma <vikass185@gmail.com>
2021-03-25 13:14:05 +0530

    setting changes

--- helpers/update-rewards.js
+++ helpers/update-rewards.js
@@ -2,8 +2,16 @@ const db = require("../models");
 const Op = db.Sequelize.Op;
 const Sequelize = db.Sequelize;
 const Users   = db.Users;
-
+const School = db.SchoolDetails;
+const Branch = db.Branch;
+/**
+ * API for update reward point
+ */
 async function updateRewardsPoints(user,points,type) {
+    let isEnabled = await isSettingEnabled(user)
+
+    if(isEnabled == 'no') return true
+
     let pointsUpdate = ""
     if(type == "increment"){
         pointsUpdate = Sequelize.literal("rewards_points + "+points)
@@ -18,6 +26,10 @@ async function updateRewardsPoints(user,points,type) {
 
 }
 
+
+/**
+ * API for get user reward point
+ */
 async function getUserRewardsPoint(user) {
     let userPoint = await Users.findOne({ 
        where: { user_name: user.userId },
@@ -27,28 +39,64 @@ async function getUserRewardsPoint(user) {
     return userPoint.rewards_points
 }
 
+
+/**
+ * API for settled redeem points
+ */
 async function updateReedeemPoint(user, points , resolved = false) {
-    if(!resolved){
-        let pointsDecrement = Sequelize.literal("rewards_points - "+points)
-        let pointsIncrement = Sequelize.literal("rewards_request + "+points)
+  let isEnabled = await isSettingEnabled(user)
 
-        await Users.update({ 
-          rewards_points  : pointsDecrement,
-          rewards_request : pointsIncrement,
-          }, 
-          { where: { user_name: user.userId }
-        });
-    }else{
-        let pointsDecrement = Sequelize.literal("rewards_request - "+points)
-        let pointsIncrement = Sequelize.literal("point_redeemed + "+points)
+  if(isEnabled == 'no') return true
 
-        await Users.update({ 
-          rewards_request  : pointsDecrement,
-          point_redeemed   : pointsIncrement,
-          }, 
-          { where: { user_name: user.userId }
-        });
+  if(!resolved){
+      let pointsDecrement = Sequelize.literal("rewards_points - "+points)
+      let pointsIncrement = Sequelize.literal("rewards_request + "+points)
+
+      await Users.update({ 
+        rewards_points  : pointsDecrement,
+        rewards_request : pointsIncrement,
+        }, 
+        { where: { user_name: user.userId }
+      });
+  }else{
+      let pointsDecrement = Sequelize.literal("rewards_request - "+points)
+      let pointsIncrement = Sequelize.literal("point_redeemed + "+points)
+
+      await Users.update({ 
+        rewards_request  : pointsDecrement,
+        point_redeemed   : pointsIncrement,
+        }, 
+        { where: { user_name: user.userId }
+      });
+  }
+}
+
+
+/**
+ * API for is setting enabled redeem points
+ */
+async function isSettingEnabled(user){
+
+    let dbUser = await Users.findOne({ 
+       where: { user_name: user.userId },
+       attributes : ['school_id','branch_vls_id']
+    });
+
+    let dbSchool = await School.findOne({ 
+       where: { school_id: dbUser.school_id },
+       attributes : ['rewards_and_recognition_support']
+    });
+
+    let dbBranch = await Branch.findOne({ 
+       where: { branch_vls_id: dbUser.branch_vls_id },
+       attributes : ['rewards_and_recognition_support']
+    });
+    
+    if(dbSchool && dbBranch){
+      if(dbSchool.rewards_and_recognition_support == 'yes' && dbBranch.rewards_and_recognition_support == 'yes')
+          return 'yes'
     }
+    return 'no'
 }
 
 module.exports = { updateRewardsPoints, getUserRewardsPoint, updateReedeemPoint };
