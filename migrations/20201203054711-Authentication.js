a04670f203e06a58030425733459a446da0dc344
vikas sharma <vikass185@gmail.com>
2021-06-14 10:28:08 +0530

    user name changes

--- migrations/20201203054711-Authentication.js
+++ migrations/20201203054711-Authentication.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_name: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       password: {
         type: Sequelize.STRING
