991299640c52628996a45c15723c497ab30edfe9
Harshit Vaid <harshitvaid000@gmail.com>
2021-10-04 18:20:06 +0530

    changes in netbanking method

--- fee_payment/app/controllers/fee.controller.js
+++ fee_payment/app/controllers/fee.controller.js
@@ -643,17 +643,17 @@ async function tansactionCheck(body){
   let paid_status = {
     paid_status: "paid"
   }
-  console.log(txStatus, "txStatustxStatustxStatustxStatus")
-  if(txStatus == "SUCCESS"){
-    console.log(orderNoteObj.payment_prefix, invoiceID,"okay in invoiceCondition")
-    let invoiceDetails = await Invoice.findOne({custom_invoice_id:invoiceID})
+
+  if(txStatus == ''){
+    let invoiceDetails = await Invoice.findOne({
+                              where :{custom_invoice_id:invoiceID}
+                           })
     await invoiceDetails.update(paid_status)
   }else{
-    console.log(orderNoteObj.payment_prefix, invoiceID,"okay in else")
     transactionObj.transaction_failed_reason = body.txMsg
   }
 
-  // let createdTransaction = await Transaction.create(transactionObj)
+  let createdTransaction = await Transaction.create(transactionObj)
   
   
   let redirectUrl = configEnv.frontendURL+"/app/payment/detail?id="+orderNoteObj.invoice_id
