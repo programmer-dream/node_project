a381f83472f6f828d58b2fb30f0f27cad6b329d2
vikas sharma <vikass185@gmail.com>
2022-06-03 11:43:43 +0530

    made lower case

--- vls/app/controllers/auth.controller.js
+++ vls/app/controllers/auth.controller.js
@@ -598,10 +598,10 @@ async function manageUserSettingsSchool(userDetails) {
 
   await Promise.all(
     permissionArray.map( item => {
-
-        if( userDetails.school[item] == null || userDetails.school[item] == 'no' ){
+        console.log(item, userDetails.school[item], "")
+        if( userDetails.school[item] == null || userDetails.school[item].toLowerCase() == 'no' ){
           userSettings[item] = 'no'
-        }else if( userDetails.school[item] == 'yes' ){
+        }else if( userDetails.school[item].toLowerCase() == 'yes' ){
           userSettings[item] = 'yes'
         }
 
